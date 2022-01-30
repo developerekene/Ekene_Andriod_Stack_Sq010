6dce317f35aebff88106948b38e33eaa03c45443
Ekene Okoli <developerekene@gmail.com>
2022-01-30 10:53:43 +0100

    Sixth Commit, Yearly Season

--- src/season/YearlySeason.java
+++ src/season/YearlySeason.java
@@ -7,7 +7,8 @@ public class YearlySeason {
         //1. Ask for the Season of the Year
         System.out.println("What season of the year are you currently in?");
         Scanner scanner = new Scanner(System.in);
-        String season = scanner.next();
+        Scanner scanner2 = new Scanner(System.in);
+        String season = scanner2.nextLine();
 
         //2. Ask for a whole number
         System.out.println("Please Pick a whole Number!");
@@ -15,7 +16,7 @@ public class YearlySeason {
 
         //3. Ask for an adjective
         System.out.println("Please provide an Adjective of your choice");
-        String adjective = scanner.nextLine();
+        String adjective = scanner2.nextLine();
         scanner.close();
 
         //4. Displays result
