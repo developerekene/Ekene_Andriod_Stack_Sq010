31af4f3266a89797c6095cc7b7885cd281c386e7
Ekene Okoli <developerekene@gmail.com>
2022-02-01 00:21:46 +0100

    Tenth Commit

--- src/programmingconceptexcersice/ProgrammingConceptExcersice.java
+++ src/programmingconceptexcersice/ProgrammingConceptExcersice.java
@@ -3,13 +3,25 @@ package programmingconceptexcersice;
 import java.util.Scanner;
 
 public class ProgrammingConceptExcersice {
-    //A security protocol for letting in only my kids into the house once it is 9:00pm
+    //A security protocol for letting in only my kids into the house.
     public static void main(String args[]){
+
+
         System.out.print("Please put in your Name!");
         Scanner scanner = new Scanner(System.in);
-        String namesOfChildren = scanner.nextLine();
+        int yourAge = scanner.nextInt();
 
+        int minimumAge = 18;
 
-
+        if(yourAge == minimumAge){
+            System.out.println("You are of age to begin driving lessons!");
+        } else if(yourAge > minimumAge){
+            System.out.println("You are of age to begin driving lessons!");
+        }else{
+            System.out.println("You are not of age!");
+        }
     }
+
+
 }
+
