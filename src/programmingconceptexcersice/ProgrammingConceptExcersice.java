ed361198dd97fa8f97e796dd215236cf789139c6
Ekene Okoli <developerekene@gmail.com>
2022-02-01 08:24:47 +0100

    Eleveth Commit

--- src/programmingconceptexcersice/ProgrammingConceptExcersice.java
+++ src/programmingconceptexcersice/ProgrammingConceptExcersice.java
@@ -3,16 +3,24 @@ package programmingconceptexcersice;
 import java.util.Scanner;
 
 public class ProgrammingConceptExcersice {
-    //A security protocol for letting in only my kids into the house.
+    //A security protocol for checking the driving age of a person
     public static void main(String args[]){
 
-
+        //Ask of an input
         System.out.print("Please put in your Name!");
+        //create a scanner to take in an input
         Scanner scanner = new Scanner(System.in);
+        //store the input
         int yourAge = scanner.nextInt();
-
+        //ste out the minimum age
         int minimumAge = 18;
 
+        /*
+            run a conditional on the input given to check if the Input age is equal or greater than the minimum age then
+            print out you are of age to begin else print out you are not of age.
+
+
+        */
         if(yourAge == minimumAge){
             System.out.println("You are of age to begin driving lessons!");
         } else if(yourAge > minimumAge){
